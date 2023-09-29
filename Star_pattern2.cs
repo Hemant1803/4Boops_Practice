62b273e580a823fe5c9187383098aae0fec59b11
Hemant1803 <hshemantsharma445@gmail.com>
2023-09-29 23:31:45 +0530

    Star Pattern2

---
+++ Star_pattern2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objec_Oriented__Programs
+{
+    internal class Star_pattern2
+    {
+        public void star_pattern2() 
+        {
+            int rows = 7; 
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows; j++)
+                {
+                    if (i == 1 || i == rows || j == 1 || j == rows)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
