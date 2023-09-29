e70269bea6daf15c1b83baaddd3becdd9c95d9c5
Hemant1803 <hshemantsharma445@gmail.com>
2023-09-30 00:03:36 +0530

    Max_Min

---
+++ Max_Min.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objec_Oriented__Programs
+{
+    internal class Max_Min
+    {
+        
+internal static void max_min()
+        {
+            int[] arr = new int[5] { 99, 95, 93, 89, 87 };
+            int i, max, min, n;
+            // size of the array
+            n = 5;
+            max = arr[0];
+            min = arr[0];
+            for (i = 1; i < n; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        Console.WriteLine("Maximum element = {0}", max);
+      Console.WriteLine("Minimum element = {0}", min);
+   }
+
+}
+    }
+        
+    }
+
