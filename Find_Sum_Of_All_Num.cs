414ee4929a3e7f75aa9f443e250ee5220bc4c889
Hemant1803 <hshemantsharma445@gmail.com>
2023-09-30 00:04:28 +0530

    Find_Sum_Of_All_Num

---
+++ Find_Sum_Of_All_Num.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objec_Oriented__Programs
+{
+    internal class Find_Sum_Of_All_Num
+    {
+       
+internal static void find_Sum_Of_All_Num()
+        {
+            int n, sum = 0, m;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+    Console.WriteLine("Sum is= "+sum);       
+     }  
+    }
+}
