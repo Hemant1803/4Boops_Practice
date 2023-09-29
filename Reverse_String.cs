6003f810e49db5c26cafaa09767c56c51758e7fc
Hemant1803 <hshemantsharma445@gmail.com>
2023-09-30 00:06:33 +0530

    Reverse_String

---
+++ Reverse_String.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objec_Oriented__Programs
+{
+    internal class Reverse_String
+    {
+       
+
+        internal static void Reverse_string()
+        {
+            Console.WriteLine("Enter a String");
+            string str = Console.ReadLine();
+            string result = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                result += str[i];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
